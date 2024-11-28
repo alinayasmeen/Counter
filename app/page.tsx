740d87b51af8b200b130806943ed60deefa97fe5
Hafiza Alina Yasmeen <140942733+alinayasmeen@users.noreply.github.com>
2024-11-28 08:05:25 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,8 @@ function Page() {
     return (
       <div className="h-full w-full">
         <div className="flex flex-col py-10 px-10 m-2 justify-center border-sky-700 border-[4px] items-center">
-            <h1 className="absolute flex mt-0 mb-10 border justify-center border-black bg-sky-300 shadow-2xl shadow-slate-800 p-4 font-extrabold text-7xl">Your Page count is {count}</h1>
-            <div className="relative flex mt-20 mb-64 border gap-5 flex-row justify-center">
+            <h1 className="flex mt-0 mb-10 border justify-center border-black bg-sky-300 shadow-2xl shadow-slate-800 p-4 font-extrabold text-7xl">Your Page count is {count}</h1>
+            <div className="flex mt-20 mb-64 border gap-5 flex-row justify-center">
             <button onClick={handleIncrement} 
             className="flex border h-10 ml-2 justify-center w-40 bg-pink-300 items-center border-black shadow-lg shadow-slate-800 text-5xl font-medium"
             >+</button>
